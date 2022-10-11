95692df0e14b8403b93c5f5adf29f2dcd1c593ae
Franciscoreach <francisco_claveria_aguila@hotmail.com>
2022-10-10 20:10:05 -0400

    Conectando Proyecto a Oracle 19c

--- feriaVIRTUAL/settings.py
+++ feriaVIRTUAL/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'feriaVIRTUAL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'orcl',
+        'USER': 'feriavirtualtest',
+        'PASSWORD': 'feriavirtualtest123',
+        'HOST': '127.0.0.1',
+        'PORT': '1521',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
